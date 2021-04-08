5a8c234476f7eb60b8f9bc5fc91e683b4a0b0b78
Natali Soares <natalisoares90@gmail.com>
2021-04-07 22:47:50 -0300

    strings com if

--- decisoes.php
+++ decisoes.php
@@ -1,9 +1,11 @@
 <?php
 
-$idade = 17; 
+$idade = 20;
+$numeroDePessoas = 1; 
 
 
-echo "Só pode entrar se tiver mais do que 18 anos." . PHP_EOL;
+echo "Só pode entrar se tiver mais do que 18 anos ou";
+echo "a partir de 16 anos acompanhado" . PHP_EOL; 
 
 if ($idade >= 18)  {
 
@@ -11,7 +13,13 @@ if ($idade >= 18)  {
     echo 'Pode entrar';
 }
 
-echo "Você só tem $idade anos." . PHP_EOL;
-echo 'Você não pode entrar';
+else {
+
+    echo "Você só tem $idade anos." . PHP_EOL;
+    echo 'Você não pode entrar';
+}
+
+echo PHP_EOL;
+echo "Adeus!";
 
 ?>
--- strings.php
+++ strings.php
@@ -8,7 +8,7 @@ echo "Eu tenho \"$idade\" anos";
 
 echo PHP_EOL . PHP_EOL;
 
-echo "Você só pode entrar, se tiver mais de 18 anos\n";
+echo "Você só pode entrar, se tiver mais de 18 anos";
 
 echo "Você tem $idade anos. Pode entrar";
 
