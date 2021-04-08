541e7047df8a93cce6a210875f87bb612250dfcf
Natali Soares <natalisoares90@gmail.com>
2021-04-07 23:26:01 -0300

    usando else e if no mesmo sentido

--- decisoes.php
+++ decisoes.php
@@ -1,24 +1,20 @@
 <?php
 
-$idade = 20;
-$numeroDePessoas = 1; 
+$idade = 18;
+$numeroDePessoas = 1 ; 
 
 
 echo "Só pode entrar se tiver mais do que 18 anos ou";
-echo "a partir de 16 anos acompanhado" . PHP_EOL; 
+echo " a partir de 16 anos acompanhado." . PHP_EOL; 
 
 if ($idade >= 18)  {
 
-    echo "Você tem $idade anos."  . PHP_EOL;
-    echo 'Pode entrar';
-}
-
-else {
-
-    echo "Você só tem $idade anos." . PHP_EOL;
-    echo 'Você não pode entrar';
-}
-
+    echo "Você tem $idade anos. Pode entrar sozinho.";
+} else if ($idade >= 16 && $numeroDePessoas > 1) {
+        echo "Você tem $idade anos, esta acompanhada(o), então pode entrar.";
+    } else    
+         echo "Você só tem $idade anos. Você não pode entrar.";
+     
 echo PHP_EOL;
 echo "Adeus!";
 
