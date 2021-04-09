8136fe7cd91c15ed8a2e88f6b07393baab68db03
Natali Soares <natalisoares90@gmail.com>
2021-04-08 22:46:13 -0300

    usando while para repetição

--- decisoes.php
+++ decisoes.php
@@ -1,7 +1,7 @@
 <?php
 
-$idade = 18;
-$numeroDePessoas = 1 ; 
+$idade = 17;
+$numeroDePessoas = 2 ; 
 
 
 echo "Só pode entrar se tiver mais do que 18 anos ou";
@@ -12,9 +12,11 @@ if ($idade >= 18)  {
     echo "Você tem $idade anos. Pode entrar sozinho.";
 } else if ($idade >= 16 && $numeroDePessoas > 1) {
         echo "Você tem $idade anos, esta acompanhada(o), então pode entrar.";
-    } else    
-         echo "Você só tem $idade anos. Você não pode entrar.";
+    } else   {  
+         echo "Você só tem $idade anos. Você não pode entrar";
      
+}
+
 echo PHP_EOL;
 echo "Adeus!";
 
---
+++ repeticao.php
@@ -0,0 +1,15 @@
+<?php
+
+$contador = 1;
+
+while ($contador <= 150) {
+    echo "#$contador" . PHP_EOL;
+    $contador = $contador + 1;
+}
+
+
+
+
+
+
+?>
