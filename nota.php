4751e0c129e3753516d7ef73095d83f527e1125b
Natali Soares <natalisoares90@gmail.com>
2021-04-11 00:23:39 -0300

    atualizando dados

--- nota.php
+++ nota.php
@@ -1,9 +1,9 @@
 <?PHP
- $nota = 10;
+ $nota = 2;
 
 if ($nota >= 7) {
     echo "Aprovado: $nota";
-} elseif (($nota <7) && ($nota>= 4)) {
+} elseif (($nota <7) && ($nota >= 4)) {
     echo "Recuperação:$nota";
 } else {
     echo "Reprovado:$nota";
