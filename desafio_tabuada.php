6773cd355c7a4d39b99a942c7c3061fdfa6bcb1d
Natali Soares <natalisoares90@gmail.com>
2021-04-10 22:16:29 -0300

    usando for no loop tabuada

--- desafio_tabuada.php
+++ desafio_tabuada.php
@@ -1,8 +1,9 @@
 <?php
 
-for ($i = 1; $i < 100; $i++) {
-    if ($i 2 != 0) {
-        echo $i . PHP_EOL;
+
+$multiplicador = 3 ;
+
+for ($i = 1; $i <= 10; $i++) {
+        echo "multiplicador x $i = " . $multiplicador * $i . PHP_EOL;
     }
-}
 ?>
