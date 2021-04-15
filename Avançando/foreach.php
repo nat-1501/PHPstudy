903758cfdd13d6ef1187274b58a97900b391c851
Natali Soares <natalisoares90@gmail.com>
2021-04-14 22:03:07 -0300

    incluindo dados na arrays

--- Avançando/foreach.php
+++ Avançando/foreach.php
@@ -2,22 +2,39 @@
 
 
 $contasCorrentes = [ 
-    12345678910 => [
+    '123.456.789-10' => [
         'titular' => 'Vinicius', 
         'saldo' =>  1000
     ], 
-    12365487888 => [
+    '123.654.878-88' => [
         'titular' => 'Maria',
         'saldo' => 10000
     ], 
-    12365445678 => [
+    '123.654.456-78' => [
         'titular' => 'Alberto',
         'saldo' => 300
+    ],
+    '132.326.459-10' => [
+        'titular' => 'Julio',
+        'saldo' => 100
+    ]
+
 
 ];
 
+ $contasCorrentes ['123.654.874-58'] = [
+ 
+        'titular' => 'Claudia',
+        'saldo' => 200
+ ];
+
+ $contasCorrentes ['325.555.265-86'] = [
+     'titular' => 'Aline',
+     'saldo' => 590
+ ];
+
 foreach ($contasCorrentes as $cpf => $conta) {
-    echo $cpf  . PHP_EOL;
+    echo $cpf . "  " . $conta ['titular']  . PHP_EOL;
 }
 
 
