483e2505cc19ecd809f6c98c8837bc60c7aae5af
Natali Soares <natalisoares90@gmail.com>
2021-04-22 23:29:18 -0300

    exercicio com laço de repetição

--- Avançando/Exercicios/quem_éo_maior_exercicio1.php
+++ Avançando/Exercicios/quem_éo_maior_exercicio1.php
@@ -1,19 +1,23 @@
 <?php
 
-echo maior ([20, 33.5, 10.8, 50, 50.9, 8]) . PHP_EOL;
-echo maior ([30, 10.5]) . PHP_EOL;     
-    function maior ($pacotes) {
-        $maior = 1;
-        for ($i=0;$i<count([$pacotes]);$i++){
+ echo maior ([20, 33.5, 10.8, 50, 50.9, 80, 8]) . PHP_EOL;
+echo maior ([30, 50, 10.5]) . PHP_EOL;     
 
-            if ($pacotes[$i] > $maior) {
-                $maior = $pacotes [$i];
+    function maior ($pacotes) {
+        
+
+        $ultimoPeso = 0;
+
+        for ($i = 0; $i < count($pacotes); $i++){
+
+            if ($pacotes[$i] > $ultimoPeso) {
+                $ultimoPeso = $pacotes[$i];
             }
     
 
 
         }
-        return $maior;
+        return $ultimoPeso;
     }
     
 
