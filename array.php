60021b721f8b754b3dd66a30ae51aa0945fb5158
Natali Soares <natalisoares90@gmail.com>
2021-05-25 17:44:15 -0300

    Iniciando com forech

--- array.php
+++ array.php
@@ -1,11 +1,20 @@
 <?php
 
-$array = array("canal", "informatica", "loja");
-print_r($array);
+$array = [
+    "1" => "Nome",
+    "2" => "Endereco",
+    "3" => "Telefone",
+    "4" => "CEP", 
+    "5" => "CPF"
+
+];
 
 
+var_dump($array);
 
+echo "<hr>";
 
+echo $array['1']; 
 
 
 ?>
